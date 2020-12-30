2ca1eb1563e28a8e335a1069dd7660cd777a1c37
Krystin Li <krystinkrystin@hotmail.com>
2020-12-30 11:35:54 -0500

    add a leetcode graph

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datetime import date,timedelta
 import matplotlib.pyplot as plt
 
+
 def import_data():
     """
     read data from csv
@@ -11,6 +12,7 @@ def import_data():
     print("The current time sheet has %d rows." % len(data))
     print(data.tail(5))
     return data 
+
 
 def insert_new_row(lst, data):
     """
@@ -27,6 +29,7 @@ def insert_new_row(lst, data):
     }, ignore_index=True,)
     return data 
 
+
 def transform_data(input_data):
     """
     create "Total" column
@@ -41,6 +44,7 @@ def transform_data(input_data):
     input_data.drop('Date', axis=1, inplace=True)
     print("After tranformed:", input_data.tail(5))
     return input_data
+
 
 def plot_static(data, colname, color):
     """
@@ -65,6 +69,31 @@ def plot_static(data, colname, color):
     plt.savefig("img/" + colname + ".png") 
     print("Generated plot for", colname)
 
+    
+def plot_leetcode():
+    """
+    plot leetcode progress 
+    save it under img/
+    """
+    x = ["2020-11", "2020-12", "2021-01", "2021-02", "2021-03",]
+    y = [18, 23, 0, 0, 0]
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(18, 5) # img size
+
+    plt.bar(x, y, 
+            color="pink", 
+            edgecolor="yellow",
+            linewidth=2,)
+
+    ax.set(xlabel = "Month", 
+           ylabel = "Number of Questions", 
+           title = "Leetcode Questions completed accumulative",)
+    
+    plt.savefig("img/" + "leetcode" + ".png") 
+    print("Generated plot for leetcode.")    
+
+    
 def main():
     
     # 1) read data
@@ -91,6 +120,9 @@ def main():
     plot_static(data, "ds_project", "yellow")
     plot_static(data, "planning", "purple")
     plot_static(data, "work", "green")
+    
+    # 5) leetcode plot
+#     plot_leetcode() # only run at month end to refresh leetcode plot
     
 if __name__ == "__main__":
     main()
