dd15a366c2e1e8b5dea3b9ebf10f11033fa21285
Krystin Li <krystinkrystin@hotmail.com>
2021-01-21 21:27:29 -0500

    daily update

--- run.py
+++ run.py
@@ -111,13 +111,28 @@ def main():
     
     # 2) add new data
     input_data = insert_new_row(
-        ["2021-01-17", 
-         0, # work
-         2.5, # ds_project
+        ["2021-01-18", 
+         3.5, # work
+         0, # ds_project
          0, # coding
          0], # planning 
         input_data,)
 
+    input_data = insert_new_row(
+        ["2021-01-19", 
+         4, # work
+         0, # ds_project
+         0, # coding
+         0], # planning 
+        input_data,)
+
+    input_data = insert_new_row(
+        ["2021-01-20", 
+         5.5, # work
+         0, # ds_project
+         0, # coding
+         0], # planning 
+        input_data,)
     
 #     input_data.loc[
 #         (input_data["Date"] == "2021-01-12"), 
