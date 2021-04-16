623e74c883b99841c76c91f3b631d1e57f5cb3ca
Krystin Li <krystinkrystin@hotmail.com>
2021-04-15 22:22:08 -0400

    revamp the data columns and update process

--- run.py
+++ run.py
@@ -1,48 +1,38 @@
 import pandas as pd
-from datetime import date,timedelta
+from datetime import date, timedelta, datetime
 import matplotlib.pyplot as plt
-
 
 def import_data():
     """
     read data from csv
+    add new data up to today
+    load updated date back to csv
     """
-    data = pd.read_csv("data/time_sheet.csv")
+    data = pd.read_csv("data/data2.csv")
     print("Refresh starting ...")
-    print("The current time sheet has %d rows." % len(data))
-    print(data.tail(5))
-    return data 
 
+    # fill up new dates
+    max_date = datetime.strptime(data.Date.max(), '%Y-%m-%d').date()
+    today = date.today()
 
-def insert_new_row(lst, data):
-    """
-    append new row to existing dataframe
-    """
-    data = data.append(
-    {
-        "Date" : lst[0],
-        "work" : lst[1],
-        "ds_project" : lst[2],
-        "coding" : lst[3],
-        "planning" : lst[4] 
-        
-    }, ignore_index=True,)
-    return data 
-
+    while max_date < today:
+        max_date = max_date + timedelta(days=1)
+        data = data.append(
+        {
+            "Date" : max_date.strftime("%Y-%m-%d"),
+            "Day" : max_date.strftime('%A'),
+            "Work" : 0,
+            "Development" : 0,
+            "Self-Care" : 0,
+        }, ignore_index=True,)
+    data.to_csv("data/data2.csv", index=False)
+    return data
 
 def transform_data(input_data):
     """
-    create "Total" column
-    save to csv
     set "Date" as index column
     """
-    input_data["Total"] = input_data["work"] + input_data["ds_project"] + input_data["coding"] + input_data["planning"] 
-    input_data.to_csv("data/time_sheet.csv", index=False)
-    
-    # combine ds_project and planning
-    input_data["Project"] = input_data["ds_project"] + input_data["planning"]
-    
-    
+    input_data = pd.read_csv("data/data2.csv")
     input_data.index = pd.to_datetime(input_data.Date) # set date as index
     input_data.sort_index(inplace=True)
     input_data.drop('Date', axis=1, inplace=True)
@@ -52,36 +42,36 @@ def transform_data(input_data):
 
 def plot_static(data, colname, color, target_low, target_high, img_name):
     """
-    plot column 
+    plot column
     save it under img/
     """
     fig, ax = plt.subplots()
     fig.set_size_inches(18, 5) # img size
-    
-    ax.plot(colname, 
-        data = data, 
-        color = "black", 
-        linewidth = 1, 
-        marker = 'o', 
+
+    ax.plot(colname,
+        data = data,
+        color = "black",
+        linewidth = 1,
+        marker = 'o',
         markeredgecolor = color,
         markersize = 3,)
 
-    ax.set(xlabel = "Date", 
-           ylabel = "Hours", 
+    ax.set(xlabel = "Date",
+           ylabel = "Hours",
            title = colname)
-    
-    # target line 
+
+    # target line
     plt.axhline(y=target_low, color='r', linestyle='dashed')
     plt.axhline(y=target_high, color='g', linestyle='dashed')
     plt.legend()
-    
-    plt.savefig("img/" + img_name + ".png") 
+
+    plt.savefig("img/" + img_name + ".png")
     print("Generated plot for", colname)
 
-    
+
 def plot_leetcode():
     """
-    plot leetcode progress 
+    plot leetcode progress
     save it under img/
     """
     x = ["2020-12", "2021-01", "2021-02", "2021-03",]
@@ -91,118 +81,44 @@ def plot_leetcode():
     fig, ax = plt.subplots()
     fig.set_size_inches(18, 5) # img size
 
-    plt.bar(x, y, 
-            color="pink", 
+    plt.bar(x, y,
+            color="pink",
             edgecolor="yellow",
             linewidth=2,)
 
-    ax.set(xlabel = "Month", 
-           ylabel = "Number of Questions", 
+    ax.set(xlabel = "Month",
+           ylabel = "Number of Questions",
            title = "Leetcode Questions completed",)
-    
-    plt.savefig("img/" + "leetcode" + ".png") 
-    print("Generated plot for leetcode.")    
 
-    
+    plt.savefig("img/" + "leetcode" + ".png")
+    print("Generated plot for leetcode.")
+
+
 def main():
-    
+
     # 1) read data
-    input_data = import_data() 
-    
-    # 2) add new data    
-    input_data = insert_new_row(
-        ["2021-03-23", 
-         5.5, # work
-         0, # ds_project
-         0, # coding
-         0], # planning 
-        input_data,)
-
-    input_data = insert_new_row(
-        ["2021-03-24", 
-         7.5, # work
-         0, # ds_project
-         0, # coding
-         0], # planning 
-        input_data,)
-  
-    input_data = insert_new_row(
-        ["2021-03-25", 
-         3, # work
-         0, # ds_project
-         0, # coding
-         0], # planning 
-        input_data,)
-    
-    input_data = insert_new_row(
-        ["2021-03-26", 
-         3.5, # work
-         0, # ds_project
-         0, # coding
-         0], # planning 
-        input_data,)
-
-    input_data = insert_new_row(
-        ["2021-03-27", 
-         0, # work
-         0, # ds_project
-         0, # coding
-         0], # planning 
-        input_data,)
- 
-    input_data = insert_new_row(
-        ["2021-03-28", 
-         0, # work
-         0, # ds_project
-         0, # coding
-         1], # planning 
-        input_data,)
-
-    
-    input_data = insert_new_row(
-        ["2021-03-29", 
-         3.5, # work
-         0, # ds_project
-         0, # coding
-         0], # planning 
-        input_data,)
-
-    input_data = insert_new_row(
-        ["2021-03-30", 
-         6, # work
-         0, # ds_project
-         0, # coding
-         0.5], # planning 
-        input_data,)
-    
-#     input_data.loc[
-#         (input_data["Date"] == "2021-03-14"), 
-#     "planning"] = 0.5 # update col
+    input_data = import_data()
 
     # 3) data transformation
     data = transform_data(input_data)
-    
-    # 4) create plot 
+
+    # 4) create plot
     data_10 = data[data.index >= pd.to_datetime(date.today() - timedelta(days=10))].copy() # 10 days
     data_30 = data[data.index >= pd.to_datetime(date.today() - timedelta(days=30))].copy() # 30 days
-    data_60 = data[data.index >= pd.to_datetime(date.today() - timedelta(days=60))].copy() # 60 days 
-    
-    # short term view 
+    data_60 = data[data.index >= pd.to_datetime(date.today() - timedelta(days=60))].copy() # 60 days
+
+    # short term view
     plot_static(data_30, "coding", "red", 0.25, 0.5, "coding")
     plot_static(data_30, "work", "green", 4, 5, "work")
     plot_static(data_30, "Project", "purple", 0.5, 1, "project")
-    
+
     # long term view
     plot_static(data_10, "Total", "blue", 5, 7, "Total_10days",)
     plot_static(data_30, "Total", "green", 5, 7, "Total_30days",)
     plot_static(data_60, "Total", "red", 5, 7, "Total_60days")
-    
+
     # 5) leetcode plot
 #     plot_leetcode() # only run at month end to refresh leetcode plot
-    
+
 if __name__ == "__main__":
     main()
-
-
-
-    
