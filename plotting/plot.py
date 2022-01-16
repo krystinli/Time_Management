3be51fc7f444d8fdb3e663a2bf2134673ba177a9
Krystin Li <krystinkrystin@hotmail.com>
2022-01-16 12:05:11 -0500

    reorg code base

--- plotting/plot.py
+++ plotting/plot.py
@@ -1,56 +1,6 @@
-import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import date, timedelta, datetime
-
-def transform_data(
-    data,
-    days_count=30,
-    # current benchmark setting for weekday
-    weekday_work_exp=2,
-    weekday_dev_exp=0.5,
-    weekday_care_exp=1,
-    # current benchmark setting for weekend
-    weekend_work_exp=0,
-    weekend_dev_exp=0.5,
-    weekend_care_exp=1.5,
-    ):
-    """
-    set "Date" as index column
-    cut date into relevant range
-    Transform the Y-axis value from hours to performance
-
-    performance = expected hours - actual hours
-    """
-    # transform y-axis from hours to performance
-    data["Work_Scaled"] = np.where(
-        (data["Day"]=="Saturday") | (data["Day"]=="Sunday"),
-        data["Work"] - weekend_work_exp,
-        data["Work"] - weekday_work_exp,)
-
-    data["Dev_Scaled"] = np.where(
-        (data["Day"]=="Saturday") | (data["Day"]=="Sunday"),
-        data["Development"] - weekend_dev_exp,
-        data["Development"] - weekday_dev_exp,)
-
-    data["Care_Scaled"] = np.where(
-        (data["Day"]=="Saturday") | (data["Day"]=="Sunday"),
-        data["Self-Care"] - weekend_care_exp,
-        data["Self-Care"] - weekday_care_exp,)
-
-    data["Total"] = data["Work"] + data["Development"] + data["Self-Care"]
-
-    # calculate the time period to plot
-    last_month = date.today() - timedelta(days=days_count)
-    data = data[
-        (data.Date >= last_month.strftime("%Y-%m-%d")) &
-        (data.Date <= date.today().strftime("%Y-%m-%d"))]
-
-    # set date as index
-    data.Date = pd.to_datetime(data.Date)
-    data.set_index("Date", inplace=True)
-    return data.sort_index()
 
 def plot_static(
         data,
@@ -96,7 +46,7 @@ def plot_body_mind():
     """
     # workout, meditation, nap
     data=[["Week 1",1,1,1], # Sunday (Starting Point)
-          ["Week 2",0,1,2], # Jan 10-16
+          ["Week 2",4,1,6], # Jan 10-16, out of 7
           ["Week 3",0,0,0],
           ["Week 4",0,0,0]]
 
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 from datetime import date, timedelta, datetime
-from plotting.plot import transform_data, plot_static, plot_body_mind
+from plotting.plot import plot_static, plot_body_mind
 import os
 
 # os.getcwd()
@@ -9,17 +10,41 @@ import os
 def import_data():
     """
     read data from csv
-    add new data up to today
-    load updated date back to csv
+    count total rows in the current dataset
     """
     # load current data
     data = pd.read_csv("data/data2.csv")
-    print("Loaded csv with %d rows. Refresh starting ..." % (len(data)))
 
-    # fill up new dates
+    print("This repo started on:", min(data.Date))
+    print("Loaded csv with %d days of data." % (len(data)))
+    print("This process been maintained for %d months. Good job!" % (len(data)/30))
+    print("Refresh starting ...")
+    print("""\
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       "\"
+                                  ,"  ## |   ಠ ಠ.
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+    """)
+    return data
+
+def update_data(data):
+    """
+    add new data up to today
+    load updated date back to csv
+    """
+    # fill up new dates up to the next day of today
     max_date = datetime.strptime(data.Date.max(), '%Y-%m-%d').date()
     today = date.today()
-    print("You have %d missing days." % ((today - max_date).days))
+
+    if (today - max_date).days > 0:
+        print("You have %d missing days." % ((today - max_date).days))
 
     while max_date <= today:
         max_date = max_date + timedelta(days=1)
@@ -37,12 +62,63 @@ def import_data():
     print("Adding new entries up to", data.Date.max())
     return data
 
-def main():
-    # 1) read and transform data
-    input_data = import_data()
-    clean_data = transform_data(input_data)
+def transform_data(
+    data,
+    days_count=30,
+    # current benchmark setting for weekday
+    weekday_work_exp=2,
+    weekday_dev_exp=0.5,
+    weekday_care_exp=1,
+    # current benchmark setting for weekend
+    weekend_work_exp=0,
+    weekend_dev_exp=0.5,
+    weekend_care_exp=1.5,
+    ):
+    """
+    set "Date" as index column
+    cut date into relevant range
+    Transform the Y-axis value from hours to performance
 
-    print(clean_data.tail(10))
+    performance = expected hours - actual hours
+    """
+    # transform y-axis from hours to performance
+    data["Work_Scaled"] = np.where(
+        (data["Day"]=="Saturday") | (data["Day"]=="Sunday"),
+        data["Work"] - weekend_work_exp,
+        data["Work"] - weekday_work_exp,)
+
+    data["Dev_Scaled"] = np.where(
+        (data["Day"]=="Saturday") | (data["Day"]=="Sunday"),
+        data["Development"] - weekend_dev_exp,
+        data["Development"] - weekday_dev_exp,)
+
+    data["Care_Scaled"] = np.where(
+        (data["Day"]=="Saturday") | (data["Day"]=="Sunday"),
+        data["Self-Care"] - weekend_care_exp,
+        data["Self-Care"] - weekday_care_exp,)
+
+    data["Total"] = data["Work"] + data["Development"] + data["Self-Care"]
+
+    # calculate the time period to plot
+    last_month = date.today() - timedelta(days=days_count)
+    data = data[
+        (data.Date >= last_month.strftime("%Y-%m-%d")) &
+        (data.Date <= date.today().strftime("%Y-%m-%d"))].copy()
+
+    # set date as index
+    data.Date = pd.to_datetime(data.Date)
+    data.set_index("Date", inplace=True)
+    return data.sort_index()
+
+def main():
+    input_data = import_data()
+    updated_data = update_data(input_data)
+    clean_data = transform_data(updated_data)
+    updated_data
+
+    print("-----------------------------------")
+    print(clean_data[["Day", "Work", "Development", "Self-Care", "Total"]].tail(5))
+    print("-----------------------------------")
 
     # 2) plotting performance
     plot_static(clean_data, "Work_Scaled", "blue", "work_plot", 0)
