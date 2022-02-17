47e662cdd7b5501b708ba8313cea786df3150698
Krystin Li <krystinkrystin@hotmail.com>
2022-02-17 11:36:21 -0500

    update Feb

--- plotting/plot.py
+++ plotting/plot.py
@@ -47,7 +47,7 @@ def plot_body_mind():
     # day of the week
     data=[["Week 1",0,0,0,1,0,0,1,],
           ["Week 2",2,1,1,2,1,1,1,],
-          ["Week 3",1,0,0,0,0,0,0,], # this week
+          ["Week 3",1,0,1,0,0,0,0,], # this week
           ["Week 4",0,0,0,0,0,0,0,]]
 
     # convert above data into pd df
