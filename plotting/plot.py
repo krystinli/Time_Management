911c236f9846d1bd23f96e55f69d2285c23c906b
Krystin Li <krystinkrystin@hotmail.com>
2022-03-05 18:14:45 -0500

    update Mar

--- plotting/plot.py
+++ plotting/plot.py
@@ -46,7 +46,7 @@ def plot_body_mind():
     This month theme: exercise!
     """
     # day of the week
-    data=[["Week 1",0,1,1,1,0,0,0,], # this week
+    data=[["Week 1",0,1,1,1,0,1,0,], # this week
           ["Week 2",0,0,0,0,0,0,0,],
           ["Week 3",0,0,0,0,0,0,0,],
           ["Week 4",0,0,0,0,0,0,0,]]
@@ -79,7 +79,7 @@ def plot_self_control():
         2) Control eating
     """
     # day of the week
-    data=[["Week 1",0,0,1,1,1,0,0,], # this week
+    data=[["Week 1",0,0,1,2,1,0,0,], # this week
           ["Week 2",0,0,0,0,0,0,0,],
           ["Week 3",0,0,0,0,0,0,0,],
           ["Week 4",0,0,0,0,0,0,0,]]
