870d5ba599739704b4785067addf3d2cbdc62a15
Krystin Li <krystinkrystin@hotmail.com>
2022-03-08 23:15:34 -0500

    update Mar

--- plotting/plot.py
+++ plotting/plot.py
@@ -80,7 +80,7 @@ def plot_self_control():
     """
     # day of the week
     data=[["Week 1",0,0,1,2,1,0,0,],
-          ["Week 2",0,0,0,0,0,0,0,], # this week
+          ["Week 2",0,4,0,0,0,0,0,], # this week
           ["Week 3",0,0,0,0,0,0,0,],
           ["Week 4",0,0,0,0,0,0,0,]]
 
