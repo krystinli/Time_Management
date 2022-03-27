2028ba0b055a8bab21fc8b83b590d704bf736d56
Krystin Li <krystinkrystin@hotmail.com>
2022-03-27 13:56:09 -0400

    update Mar

--- plotting/plot.py
+++ plotting/plot.py
@@ -49,7 +49,7 @@ def plot_body_mind():
     data=[["Week 1",0,1,1,1,0,1,0,],
           ["Week 2",1,1,0,0,1,0,0,],
           ["Week 3",1,0,0,1,0,0,1,],
-          ["Week 4",1,0,0,0,0,0,0,]] # this week
+          ["Week 4",1,0,0,0,0,0,1,]] # this week
 
     # convert above data into pd df
     df=pd.DataFrame(
