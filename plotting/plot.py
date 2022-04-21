12e64107c312709fc3fac2d25cca838a9d776e13
Krystin Li <krystinkrystin@hotmail.com>
2022-04-21 19:24:17 -0400

    update Apr

--- plotting/plot.py
+++ plotting/plot.py
@@ -48,7 +48,7 @@ def plot_exercise():
     # day of the week
     data=[["Week 1",0,1,2,0,1,0,1,],
           ["Week 2",0,0,1,2,1,0,1,],
-          ["Week 3",0,0,0,0,0,0,0,], # this week
+          ["Week 3",0,1,0,0,0,0,0,], # this week
           ["Week 4",0,0,0,0,0,0,0,],
           ["Week 5",0,0,0,0,0,0,0,],
           ]
