f7ff5b1025295e9ae3fc71cf3aaf66efc3ca76ed
krystinli <krystinkrystin@hotmail.com>
2025-09-11 22:37:10 -0400

    refactor

--- run.py
+++ run.py
@@ -3,24 +3,22 @@ import pandas as pd
 import numpy as np
 from datetime import date, timedelta, datetime as dt
 from plotting.plot import (
-        plot_month_trend,
-        plot_day_trend,
-        plot_stacked_bar,
-    )
+    plot_month_trend,
+    plot_day_trend,
+    plot_stacked_bar,
+)
+
 
 def import_data():
-    """Read data from csv. Prints summary stats of the current dataset and a
-    cute giraffe ascii art.
-
-    Parameters
-    ----------
+    """
+    Read data from a CSV file. Prints summary stats of the current dataset and a
+    cute giraffe ASCII art.
 
     Returns
     -------
-    data : DataFrame
-        Current data before the new update
+    pd.DataFrame
+        Current data before the new update.
     """
-    # Get the file path from an environment variable or use the default
     file_path = os.getenv("DATA_FILE_PATH", "data/data2.csv")
 
     try:
@@ -35,110 +33,107 @@ def import_data():
         print(f"An unexpected error occurred: {e}")
         return pd.DataFrame()
 
+    total_days = len(data)
+    years, remaining_days = divmod(total_days, 365)
+    months, days = divmod(remaining_days, 30)
+
     print("This repo started on:", min(data.Date))
-    print("Loaded csv with %d days of data." % (len(data)))
-    print("This process been maintained for %d months. Good job!" % (len(data)/30))
+    print(f"Loaded CSV with {years} years, {months} months, and {days} days of data.")
+    print(f"This process has been maintained for {total_days // 30} months. Good job!")
     print("Refresh starting ...")
-    print("""\
+    print(
+        """\
                                        ._ o o
-                                       \_`-)|_
-                                    ,""       "\"
+                                       \\_`-)|_
+                                    ,""       "\\"
                                   ,"  ## |   ಠ ಠ.
                                 ," ##   ,-\__    `.
                               ,"       /     `--._;)
-    """)
+        """
+    )
     return data
 
+
 def update_data(data):
-    """Add new data up to today and load updated date back to csv.
+    """
+    Add new data up to today and load updated data back to the CSV file.
 
     Parameters
     ----------
-    data : DataFrame
-        Current data before the new update
+    data : pd.DataFrame
+        Current data before the new update.
 
     Returns
     -------
-    data : DataFrame
-        Updated data with the new entires up to the run date (today)
+    pd.DataFrame
+        Updated data with the new entries up to the run date (today).
     """
-    # fill up new dates up to the next day of today
-    max_date = dt.strptime(data.Date.max(), '%Y-%m-%d').date()
+    max_date = dt.strptime(data.Date.max(), "%Y-%m-%d").date()
     today = date.today()
 
     if (today - max_date).days > 0:
-        print("You have %d missing days." % ((today - max_date).days))
+        print(f"You have {(today - max_date).days} missing days.")
 
-    while max_date <= today:
-        max_date = max_date + timedelta(days=1)
-        data = pd.concat([
-            data,
-            pd.DataFrame([{
-                "Date": max_date.strftime("%Y-%m-%d"),
-                "Day": max_date.strftime('%A'),  # day of the week
-                "Work": 0,
-                "Development": 0,
-                "Self-Care": 0,}])
-            ])
+        new_dates = pd.date_range(start=max_date + timedelta(days=1), end=today)
+        new_entries = pd.DataFrame({
+            "Date": new_dates.strftime("%Y-%m-%d"),
+            "Day": new_dates.strftime("%A"),
+            "Work": 0,
+            "Development": 0,
+            "Self-Care": 0,
+        })
+        data = pd.concat([data, new_entries], ignore_index=True)
 
-    # export updated data
     data.fillna(0).to_csv("data/data2.csv", index=False)
     print("Adding new entries up to", data.Date.max())
     return data
 
+
 def transform_data(
-    data,
-    # current benchmark setting for weekday
-    weekday_work_exp=5,
-    weekday_dev_exp=0.5,
-    weekday_care_exp=0.5,
-    # current benchmark setting for weekend
-    weekend_work_exp=0.5,
-    weekend_dev_exp=0.5,
-    weekend_care_exp=2,
-    ):
-    """Transform the Y-axis value from hours to a performance scale:
-    performance = expected hours - actual hours
+        data,
+        weekday_work_exp=5,
+        weekday_dev_exp=0.5,
+        weekday_care_exp=0.5,
+        weekend_work_exp=0.5,
+        weekend_dev_exp=0.5,
+        weekend_care_exp=2,
+):
+    """
+    Transform the Y-axis value from hours to a performance scale:
+    performance = expected hours - actual hours.
 
     Parameters
     ----------
-    data : DataFrame
-        Updated data with the new entires
+    data : pd.DataFrame
+        Updated data with the new entries.
 
     Returns
     -------
-    data : DataFrame
+    pd.DataFrame
         Updated data with 4 new columns:
-        Work_Scaled, Dev_Scaled, Care_Scaled, Total
+        Work_Scaled, Dev_Scaled, Care_Scaled, Total.
     """
-    #
-    # for col in ["Work", "Development", "Self-Care"]:
-    #     pd.to_numeric(data[col], errors='ignore')
-
     data["Total"] = data["Work"] + data["Development"] + data["Self-Care"]
 
-    # scaled metrics
     data["Work_Scaled"] = np.where(
         (data["Day"] == "Saturday") | (data["Day"] == "Sunday"),
-    data["Work"] - weekend_work_exp,
-    data["Work"] - weekday_work_exp,
+        data["Work"] - weekend_work_exp,
+        data["Work"] - weekday_work_exp,
     )
 
     data["Dev_Scaled"] = np.where(
         (data["Day"] == "Saturday") | (data["Day"] == "Sunday"),
-    data["Development"] - weekend_dev_exp,
-    data["Development"] - weekday_dev_exp,
+        data["Development"] - weekend_dev_exp,
+        data["Development"] - weekday_dev_exp,
     )
 
     data["Care_Scaled"] = np.where(
         (data["Day"] == "Saturday") | (data["Day"] == "Sunday"),
-    data["Self-Care"] - weekend_care_exp,
-    data["Self-Care"] - weekday_care_exp,
+        data["Self-Care"] - weekend_care_exp,
+        data["Self-Care"] - weekday_care_exp,
     )
 
-    # set date as datetime obj
     data.Date = pd.to_datetime(data.Date)
-    data = data[data.Date < pd.to_datetime(date.today())].copy()
     return data
 
 def main():
@@ -147,32 +142,23 @@ def main():
     clean_data = transform_data(updated_data)
 
     print("-----------------------------------")
-    print(clean_data[
-            ["Date", "Day", "Work", "Development", "Self-Care", "Total"]
-        ].tail(5))
+    print(clean_data[["Date", "Day", "Work", "Development", "Self-Care", "Total"]].tail(5))
     print("-----------------------------------")
 
-    # 2) plotting
-    # colours: https://matplotlib.org/3.5.0/_images/sphx_glr_named_colors_003_2_0x.png
-
-    # Work_delta
     plot_day_trend(clean_data, "Work_Scaled", "blue", "work_plot", 0)
     plot_month_trend(clean_data, "Work", "steelblue", "deepskyblue", "work_plot_monthly")
 
-    # Dev_delta
     plot_day_trend(clean_data, "Dev_Scaled", "red", "dev_plot", 0)
     plot_month_trend(clean_data, "Development", "darkseagreen", "palegreen", "dev_plot_monthly")
 
-    # Care_delta
     plot_day_trend(clean_data, "Care_Scaled", "green", "care_plot", 0)
     plot_month_trend(clean_data, "Self-Care", "palevioletred", "deeppink", "care_plot_monthly")
 
-    # Total_abs
-    plot_day_trend(clean_data, "Total", "yellow", "total_plot", 5) # avg daily expectation
+    plot_day_trend(clean_data, "Total", "yellow", "total_plot", 5)
     plot_month_trend(clean_data, "Total", "goldenrod", "gold", "total_plot_monthly")
 
-    # Others
     plot_stacked_bar()
+
 
 if __name__ == "__main__":
     main()
